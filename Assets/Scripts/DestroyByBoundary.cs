581f608da92e190457abd9746c99772ca7597938
kardoud44 <mathkardous@gmail.com>
2017-09-13 16:42:26 -0400

    Weapon Trigger Script- Start

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class DestroyByBoundary : MonoBehaviour {
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("On Trigger Enter");
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("On trigger Exit");
+    }
 }
