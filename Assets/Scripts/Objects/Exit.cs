033f169980a3c0e947848d5ba217b378fbf88062
kardoud44 <mathkardous@gmail.com>
2017-10-02 09:25:43 -0400

    Exit prefab

--- Assets/Scripts/Objects/Exit.cs
+++ Assets/Scripts/Objects/Exit.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class Exit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
 }
